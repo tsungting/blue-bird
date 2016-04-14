c89207c5767ea4f36c09c1a622984db49206f6d9
Evan Schultz <evan@rangle.io>
2016-04-14 17:38:07 -0400

    Use ng2-redux 2.2 beta

--- src/store/configure-store.ts
+++ src/store/configure-store.ts
@@ -2,7 +2,8 @@
 
 import {createStore, applyMiddleware, compose} from 'redux';
 import {fromJS} from 'immutable';
-import ReduxThunk from 'redux-thunk';
+//import ReduxThunk from 'redux-thunk';
+const ReduxThunk = require('redux-thunk').default;
 import logger from './configure-logger';
 import promiseMiddleware from '../middleware/promise-middleware';
 import rootReducer from '../reducers';
