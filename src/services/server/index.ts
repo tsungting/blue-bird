ede0d2f6d137ad1ebd56669ca3344e4f65c94c93
Marcin Bogusz <marcin@bogusz.cc>
2016-06-26 03:57:27 +0200

    data stringify on PUT method (#164)

--- src/services/server/index.ts
+++ src/services/server/index.ts
@@ -1,5 +1,5 @@
-import { Injectable, provide } from '@angular/core';
-import {Http, Request, Response, Headers} from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 const HEADERS = new Headers({ 'Content-Type': 'application/json' });
@@ -9,27 +9,25 @@ export class ServerService {
 
   private BASE_URL = '/api';
 
-  constructor(
-    private _http: Http
-  ) {}
+  constructor(private _http: Http) {}
 
   public post(path, data) {
-    return this._http.post(this.BASE_URL + path, JSON.stringify(data),
+    return this._http.post(`${this.BASE_URL}${path}`, JSON.stringify(data),
       { headers: HEADERS})
      .map((res: Response) => res.json());
   }
 
   public get(path) {
-    return this._http.get(this.BASE_URL + path)
+    return this._http.get(`${this.BASE_URL}${path}`)
     .map((res: Response) => res.json());
   }
 
   public put(path, id, data) {
-    return this._http.put(this.BASE_URL + path + '/' + id, data)
+    return this._http.put(`${this.BASE_URL}${path}/${id}`, JSON.stringify(data))
     .map((res: Response) => res.json());
   }
 
   public delete(path, id) {
-    return this._http.delete(this.BASE_URL + path + '/' + id);
+    return this._http.delete(`${this.BASE_URL}${path}/${id}`);
   }
 }
