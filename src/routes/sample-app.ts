ec4f50f93ccb5a3eac27ab4337e7132bb71d2f0b
Andrew Lo <andrew.lo@rangle.io>
2016-07-11 12:28:27 -0400

    Have counter page use the route name "counter" and redirect to (#209)
    counter page as the default page.

--- src/routes/sample-app.ts
+++ src/routes/sample-app.ts
@@ -1,8 +1,14 @@
+import { RouterConfig } from '@angular/router';
+
 import { RioCounterPage } from '../containers/counter-page';
 import { RioAboutPage } from '../containers/about-page';
 
-export const SAMPLE_APP_ROUTES = [{
+export const SAMPLE_APP_ROUTES: RouterConfig = [{
+  pathMatch: 'full',
   path: '',
+  redirectTo: 'counter'
+}, {
+  path: 'counter',
   component: RioCounterPage
 }, {
   path: 'about',
